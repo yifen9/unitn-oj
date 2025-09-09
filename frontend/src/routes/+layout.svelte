9256bad44362283e69e1abb512a65422bf372923
yifen9 <mail@yifen9.li>
2025-09-09 11:42:28 +0200

    tailwind css + daisyui + Hello, World!

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+import "../app.css";
+</script>
+
+<div class="min-h-dvh bg-base-100 text-base-content">
+	<slot />
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+const msg: string = "Hello, unitn-oj";
+</script>
+
+<main class="container mx-auto p-8">
+	<h1 class="text-3xl font-bold">{msg}</h1>
+</main>
